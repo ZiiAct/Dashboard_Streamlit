88e6934c9b17eb155269fa3a2abb7e466203aab5
ZiiAct <ziyadulq123@gmail.com>
2026-05-01 06:18:53 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import streamlit as st
 import os
 
 st.set_page_config(page_title="Air Quality Dashboard", layout="wide")
